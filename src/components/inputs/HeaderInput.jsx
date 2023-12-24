27b6f72fa339406390ee650c2084953a888c99fe
yeonji <yeonji.qp@gmail.com>
2023-12-24 17:33:57 +0900

    header input 컴포넌트 템플릿#1

---
+++ src/components/inputs/HeaderInput.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { GoSearch } from 'react-icons/go';
+
+export const HeaderInput = ({ state, placeholder }) => {
+  // const type = type;
+  return (
+    <div className={`header__input-wrapper${state && `--${state}`}`}>
+      <div className="header__input-container">
+        <i className="header__input-icon">
+          <GoSearch />
+        </i>
+        <input className="header__input" placeholder={placeholder} />
+      </div>
+      <button className="header__input-btn" type="button">
+        <i className="header__input-btn-icon">
+          <GoSearch />
+        </i>
+      </button>
+    </div>
+  )
+}
+
+HeaderInput.propTypes = {
+  state: PropTypes.bool,
+}
+
+HeaderInput.defaultProps = {
+  state: false,
+};
---
+++ src/components/inputs/HeaderInput.stories.js
@@ -0,0 +1,13 @@
+import { HeaderInput } from "./HeaderInput";
+
+export default {
+  title: "Components/Input/HeaderInput",
+  component: HeaderInput,
+}
+
+export const Primary = {
+  args: {
+    placeholder: "검색",
+    state: '',
+  }
+}
