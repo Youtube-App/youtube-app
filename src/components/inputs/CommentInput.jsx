30a9dff28dc4dbde252b0703814652ecae18b673
yeonji <yeonji.qp@gmail.com>
2023-12-31 14:27:59 +0900

    comment input의 textarea 높이 유동적으로 조정되게 수정

--- src/components/inputs/CommentInput.jsx
+++ src/components/inputs/CommentInput.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const CommentInput = ({ placeholder }) => {
+  function resizeTextarea() {
+    let textarea = document.querySelector('.comment__input');
+
+    if (textarea) {
+      textarea.style.height = '20px';
+      let height = textarea.scrollHeight;
+      textarea.style.height = `${height}px`;
+    }
+  }
   return (
     <div className="comment__input-wrapper">
-      <textarea className="comment__input" placeholder={placeholder} />
+      <textarea
+        className="comment__input"
+        placeholder={placeholder}
+        onInput={resizeTextarea}
+      />
       {/* <div className="comment__btn-wrapper">
         <button className="comment__emoji-btn" type="button" />
         <div className="comment__btn-section">
