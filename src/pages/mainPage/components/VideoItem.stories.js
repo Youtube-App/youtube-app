59d898683ed74e5ade41737534cfbd1f2ba2b713
KangMin-Ji <wax227@naver.com>
2024-01-14 13:28:26 +0900

    변수명 Primary -> Default로 변경

--- src/pages/mainPage/components/VideoItem.stories.js
+++ src/pages/mainPage/components/VideoItem.stories.js
@@ -1,26 +1,10 @@
 import { VideoItem } from 'pages/mainPage/components/VideoItem';
-// import { withRouter } from 'storybook-addon-react-router-v6';
-// import { withRouter } from 'storybook-addon-react-router-v6';
-// import { withRouter } from 'storybook-addon-react-router-v6';
 
 export default {
   title: 'Pages/MainPage/Components/VideoItem',
   component: VideoItem,
-  // decorators: [withRouter],
-  // parameters: {
-  //   reactRouter: {
-  //     routePath: '/channel/:channelId',
-  //     routeParams: { channelId: 'df23' },
-  //   },
-  // },
 };
 
-export const Primary = {
+export const Default = {
   args: {},
-  // parameters: {
-  //   reactRouter: {
-  //     routerPath: '/channel',
-  //     routerState: { channelId: 'dfdf' },
-  //   },
-  // },
 };
