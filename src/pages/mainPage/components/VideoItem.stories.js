d5da1ff4d1a436272f8918122f6776b06366d51f
KangMin-Ji <wax227@naver.com>
2024-01-19 19:02:39 +0900

    videoItem 정리

--- src/pages/mainPage/components/VideoItem.stories.js
+++
@@ -1,8 +0,0 @@
-import { VideoItem } from 'pages/mainPage/components/VideoItem';
-
-export default {
-  title: 'Pages/MainPage/Components/VideoItem',
-  component: VideoItem,
-};
-
-export const Default = {};
