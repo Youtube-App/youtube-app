42a3b63f195d9325ef5c6692be90deb32f1d5f94
KangMin-Ji <wax227@naver.com>
2024-01-28 14:04:37 +0900

    구독 버튼 제작 완료

--- src/components/buttons/SubscriptionBtn.jsx
+++ src/components/buttons/SubscriptionBtn.jsx
@@ -1,60 +1,42 @@
 import { Player } from '@lottiefiles/react-lottie-player';
 import subscription from 'json/subscription.json';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 // import { useRef } from 'react';
 // import { useRef } from 'react';
+import cn from 'classnames';
+import { SlArrowDown } from 'react-icons/sl';
 
 export const SubscriptionBtn = () => {
-  const subscriptionBtnTxt = useRef();
-  const subscriptionLottie = useRef();
-  const ad = document.getElementsByClassName('subscription__btn-text');
-  const width = ad.offsetWith;
-  const [isInvisible, setIsInvisible] = useState(false);
-  const [style, setStyle] = useState(width);
-  // const txt = document.getElementsByClassName('subscription__btn-text');
-
-  useEffect(() => {
-    // console.log(width);
-    setStyle(
-      isInvisible
-        ? subscriptionLottie.offsetWith
-        : subscriptionBtnTxt.offsetWith,
-    );
-  }, [isInvisible, style]);
+  const subscriptionLottie = useRef(null);
+  const [label, setlabel] = useState('구독');
+  const [isActive, SetIsActive] = useState(false);
 
   const onClick = () => {
-    console.log('click');
-    setIsInvisible(true);
+    setlabel('구독중');
+    SetIsActive(true);
+    subscriptionLottie.current.play();
   };
 
   return (
     <button
-      className="subscription__btn"
+      className={cn('subscription__btn', {
+        'subscription__btn-active': isActive,
+      })}
       onClick={onClick}
-      style={{ width: `${style}px` }}
     >
-      <div className="subscription__btn-wrap">
-        <span
-          className={`subscription__btn-text ${
-            isInvisible ? 'subscription__btn-text-invisible' : ''
-          }`}
-          ref={subscriptionBtnTxt}
-        >
-          구독
-        </span>
-        <div
-          className="subscription__btn-lottie-wrap"
-          ref={subscriptionLottie}
-        >
-          <div className="subscription__btn-lottie">
-            <Player
-              autoplay
-              src={subscription}
-            />
-          </div>
-          <span className="subscription__btn-text">구독중</span>
+      <div className="subscription__btn-lottie-wrap">
+        <div className="subscription__btn-lottie">
+          <Player
+            autoplay={isActive}
+            src={subscription}
+            ref={subscriptionLottie}
+          />
         </div>
       </div>
+      <span className="subscription__btn-text">{label}</span>
+      <i className="subscription__btn-icon">
+        <SlArrowDown />
+      </i>
     </button>
   );
 };
